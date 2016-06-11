1b16e754fe08536f76034ef2b7086ef19fd94de6
Kasper Guldmann Nielsen <ms@kalleguld.dk>
2016-06-11 21:05:44 +0200

    initial commmmit

---
+++ BrockTest20160611/Main.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrockTest20160611
+{
+    class Main
+    {
+
+        public void main()
+        {
+            //Not a real main method, can't be bothered to change it now
+
+            object myNormalObject = new object();
+            //myNormalObject.a = 5;
+            //compile-fejl, object har ikke en a-property
+
+            dynamic myDynamicObject = new object();
+            myDynamicObject.a = 5;
+            //vi har sagt at vi ved bedre end compileren, så den brokker sig ikke.
+            //men vi får stadig runtime-fejl
+
+
+            MySuperClass mySuperClassObject = new MySuperClass();
+            //mySuperClassObject.a = 5;
+            //MySuperClass har ikke en a-property
+
+            dynamic myDynamicSuperClassObject = new MySuperClass();
+            myDynamicSuperClassObject.a = 5;
+            //endelig virker det. 
+
+        }
+    }
+}
---
+++ BrockTest20160611/MySuperClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrockTest20160611
+{
+    public class MySuperClass : DynamicObject
+    {
+        private readonly Dictionary<string, object> _dict = new Dictionary<string, object>();
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            return _dict.TryGetValue(binder.Name, out result);
+        }
+
+        public override bool TrySetMember(SetMemberBinder binder, object value)
+        {
+            _dict[binder.Name] = value;
+            return true;
+        }
+    }
+}
